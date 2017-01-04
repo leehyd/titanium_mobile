1113fefd547c8906269907489d8c303d3c4e5711
BAEKYU PARK <ca@nablecomm.com>
2017-01-04 13:47:15 +0900

    [Android] listview를 종료하는 과정에 setSelection이 호출되었을 때 null 처리 추가

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
@@ -1040,7 +1040,9 @@ public class TiListView extends TiUIView implements OnSearchChangeListener {
 					@Override
 					public void run() 
 					{
-						listView.setSelection(position + 1);
+						if(listView != null) {
+							listView.setSelection(position + 1);
+						}
 					}
 				});
 			}
